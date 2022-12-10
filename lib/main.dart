c7ca490dd97a9dfd344e64ec102965ce00c51a48
Amu <Amansis.g2@gmail.com>
2022-12-10 16:09:32 +0300

    UI part finished

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
       home: bmi(),
@@ -28,84 +26,105 @@ class bmi extends StatefulWidget {
   State<bmi> createState() => _bmiState();
 }
 
- 
 class _bmiState extends State<bmi> {
+  double bmiresult = 0;
+  String description = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-          title:Container(
-              child: Text("BMI calculator"))
-    ),
-        body:Container(
-
-          alignment: Alignment.center,
-
-          child:Column(
-            children: <Widget>[
-             Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-
-                    width:150,
-                    height:100,
-
-                    child:  TextField(
-                      keyboardType:
-                      TextInputType.numberWithOptions(decimal: true),
-                      style: TextStyle(color: Colors.blueAccent),
-                      decoration: InputDecoration(
-                          prefixText: "Weight:",
-                        ),
-                      onChanged: (String Value) {
-                        try {
-                          debugPrint("clicked");
-                        } catch (exception) {
-
-                        }
-                      },
-
-                    ),
-                  ),
-                  Container(
-                    width:150,
-                    height:100,
-                    child:  TextField(
-                      keyboardType:
-                      TextInputType.numberWithOptions(decimal: true),
-                      style: TextStyle(color: Colors.blueAccent),
-                      decoration: InputDecoration(
-                          prefixText: "height:"),
-                      onChanged: (String Value) {
-                        try {
-                          debugPrint("clicked");
-                        } catch (exception) {
-
-                        }
-                      },
-
-                    ),
-                  ),
-
-
-
-                ],
-              ),
-              ButtonBar(buttonHeight: 20,
-              buttonMinWidth: 50,
-              buttonTextTheme: Colors.black,
+        appBar: AppBar(
+          title: Text("BMI calculator"),
+          centerTitle: true,
+        ),
+        body: Container(
+            alignment: Alignment.center,
+            child: Column(
               children: <Widget>[
-                Text("calculate")
-              ],)
-
-            ],
-          )
-        )
-    );
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Container(
+                      width: 140,
+                      child: TextField(
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.blueAccent,
+                        ),
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Height",
+                            hintStyle: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.blueGrey.withOpacity(.9))),
+                      ),
+                    ),
+                    Container(
+                      width: 140,
+                      child: TextField(
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.blueAccent,
+                        ),
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "weight",
+                            hintStyle: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.blueGrey.withOpacity(.9))),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  child: Text(
+                    "Calculate",
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.blueAccent,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  child: Text(
+                    "21",
+                    style: TextStyle(
+                      fontSize: 70,
+                      color: Colors.blueAccent,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  child: Text(
+                    "normal ",
+                    style: TextStyle(
+                      fontSize: 35,
+                      color: Colors.blueAccent,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ),
+              ],
+            )));
   }
 }
-
-
-
